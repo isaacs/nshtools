447396cd1a821121eb5cb51abad19caa63abc9ee
R. S. Doiel <rsdoiel@gmail.com>
2010-02-27 11:07:40 -0800

    updated install script to include installing syncme.js and making it executable.

--- install-nshtools.js
+++ install-nshtools.js
@@ -4,6 +4,7 @@
 nshtools = require(process.cwd() + '/nshtools');
 nsh = nshtools.createNshtool();
 nsh.getOption('--prefix', function(prefix_error, prefix_path) {
+  nsh.bin_path = '/bin';
   if (prefix_error) {
     nsh.echo('Using default install prefix: ' + nsh.env.HOME);
     nsh.prefix_path = nsh.env.HOME;
@@ -23,6 +24,29 @@ function (response) {
         nsh.echo("Install failed. " + cp_error);
         return;
       }
+      nsh.echo("Install complete. " + new Date());
+    });
+  } else {
+    nsh.echo("\n\n\tNo action taken. Nothing installed. " + new Date());
+  }
+});
+nsh.prompt("Do you want to install syncme.js in " + nsh.prefix_path +
+nsh.bin_path + "? (Y/N) ",
+function(response) {
+  if (response.toUpperCase().trim() === 'Y') {
+    nsh.verbose = true;
+    nsh.echo("\n\n\tInstalling ..." + new Date());
+    nsh.cp("syncme.js", nsh.prefix_path + nsh.bin_path + '/syncme.js', function (cp_error) {
+      if (cp_error) {
+        nsh.echo("Install failed. " + cp_error);
+        return;
+      }
+      nsh.chmod(nsh.prefix_path + nsh.bin_path + '/syncme.js', 0775, function(error) {
+        if (error) {
+          nsh.die("ERROR: installing syncme.js: " + error);
+        }
+        nsh.echo("syncme.js made executable.");
+      });
       nsh.echo("Install complete. " + new Date());
     });
   } else {
