071f18afefb378271473a2a4f11d83a5e3ff654b
R. S. Doiel <rsdoiel@gmail.com>
2010-02-27 10:39:04 -0800

    Added syncme.js, syncme.asciidoc as example tools built on nshtools.js

--- syncme.js
+++ syncme.js
@@ -5,47 +5,125 @@
  *
  *
  */
-if (process.paths.indexOf('.') < 0) {
-  process.paths.push('.');
-}
-
-var nshtools = require('nshtools'),
-    nsh = nshtools.createNshtool(),
-    syncme_conf = nsh.env.HOME + './.syncme.conf',
-    TackedRepos = {}, USAGE;
-
-USAGE = "\n\tNAME: syncme.js\n" +
-        "\n\n\tProcess a list of saved git repo clones and do a git pull.\n" +
-        "
-
-
-/* Before doing anything read in .syncme.conf or create it. */
-nsh.readFile(syncme_conf, function(read_error, data) {
-  if (stat_error) {
-    nsh.writeFile(syncme_conf, JSON.stringify(TrackedRepos), 'utf8', function(write_error) {
-      nsh.die("Can't read/write " + syncme_conf + ": " + write_error, 1);
-      nsh.echo("Created " + syncme_conf);
-    });
-  }
+(function () {
+  var nsh = require('nshtools').createNshtool(),
+      syncme_conf = nsh.env.HOME + '/.syncme',
+      TrackedRepos = {},
+      ABOUT = "NAME\n" +
+  "\tsyncme.js - utility to do a git-pull on a list of cloned repositories\n" +
+  "\nSYNOPSIS\n\n" +
+  "syncme.js is an example script that reads $HOME/.syncme to get a list of git\n" +
+  "repos and do a git pull to update the local contents.  If $HOME/.syncme\n" +
+  "doesn't exist it will try to create an empty one. You edit syncme's\n" +
+  "configuration with syncme. There are four basic options.\n\n" +
+  "\t--add=NICKNAME:PATH\n" +
+  "\t\tAdd a nickname and git repository clone's path to syncme\n\n" +
+  "\t--remove=NICKNAME\n" +
+  "\t\tRemove a nickname and it's path from syncme\n\n" +
+  "\t--view\n" +
+  "\t\tView a list of nicknames and paths syncme knows about\n\n" +
+  "\t--run\n" +
+  "\t\tRun git pull in all the locations tha syncme knows about\n\n" +
+  "\t--help\n" + 
+  "\t\tShow this help message.\n\n" +
+  "If no options are specified syncme.js assumes the --run option.\n";
   
-  nsh.getOption('--add', function (add_error, arg) {
-    var parts, key, value;
-    if (arg.indexOf(':') < 0) {
-      nsh.die(USAGE, 1);
+  
+  /* Before doing anything read in .syncme.conf or create it. */
+  nsh.getOption('--help', function(help_error, arg) {
+    if (help_error) {
+      /* Help not requested so skip */
+      return;
     }
-    nsh.echo("Adding " + parts[1] + ' as ' + parts[0] + ' to ' + synceme_conf);
-  });
-  nsh.getOption('--remove', function (add_error, value) {
-    nsh.echo("Removing " + value + ' to ' + synceme_conf);
+    nsh.die(ABOUT,0);
   });
 
-});
+  /* Push --run if no args are given to syncme.js */
+  if (nsh.ARGV.length < 3) {
+    nsh.ARGV.push('--run');
+  }
 
-
-
-
-nsh.echo("Syncing git-repos.");
-
-nsh.task("Sync node", function (){
-  nsh.chdir
-});
+  nsh.readFile(syncme_conf, function(read_error, data) {
+    if (read_error) {
+      nsh.writeFile(syncme_conf, JSON.stringify(TrackedRepos), 'utf8', 
+      function(write_error) {
+        if (write_error) {
+          nsh.die("Can't read/write " + syncme_conf + ": " + write_error, 1);
+        }
+        nsh.echo("Created " + syncme_conf);
+      });
+    }
+    TrackedRepos = JSON.parse(data);
+  
+    nsh.getOption('--add', function (option_error, arg) {
+      var pair, key = 0, value = 1;
+      if (option_error) {
+        /* Add option not selected, skip */
+        return;
+      }
+      if (arg.indexOf(':') < 0) {
+        nsh.die(ABOUT + "ERROR: You need to specify a nickname and path separated by a colon", 1);
+      }
+      pair = arg.split(':', 2);
+      nsh.echo("Added " + pair[value] + ' as ' + pair[key]);
+      TrackedRepos[pair[key]] = pair[value];
+      nsh.writeFile(syncme_conf, JSON.stringify(TrackedRepos), 
+      function(error) {
+        if (error) {
+          nsh.die("ERROR: Can't write " + syncme_conf + ": " + error);
+        }
+      });      
+    });
+    nsh.getOption('--remove', function (option_error, value) {
+      if (option_error) {
+        /* Add option not selected, skip */
+        return;
+      }
+      if (arg.indexOf(':') < 0) {
+        nsh.die(ABOUT + "ERROR: You need to specify a nickname and path separated by a colon", 1);
+      }
+      nsh.echo("Removed " + arg);
+      if (TrackedRepos[arg] === undefined) {
+        nsh.die("ERROR: Can't find " + arg + " to remove.");
+      }
+      delete TrackedRepos[arg];
+      nsh.writeFile(syncme_conf, JSON.stringify(TrackedRepos), 
+      function(error) {
+        if (error) {
+          nsh.die("ERROR: Can't write " + syncme_conf + ": " + error);
+        }
+      });      
+    });
+    nsh.getOption('--view', function (option_error, value) {
+      if (option_error) {
+        /* Add option not selected, skip */
+        return;
+      }
+      for(i in TrackedRepos) {
+        nsh.echo(i + "\n\t" + TrackedRepos[i]);
+      }
+    });
+    nsh.getOption('--run', function (option_error, value) {
+      if (option_error) {
+        /* Add option not selected, skip */
+        return;
+      }
+      for(i in TrackedRepos) {
+        (function(i, path) {
+          nsh.task(i, function () {
+            nsh.echo("Syncing: " + path);
+            nsh.exec("cd " + path + ";git pull", function(error, stdout, stderr) {
+              if (error) {
+                nsh.die("ERROR: syncme.js: cd " + path + ";git pull\n" + error);
+              }
+              nsh.echo(stdout);
+              nsh.echo(stderr);
+            });
+          });
+        })(i,TrackedRepos[i]);
+        /* We have a tasks all lined up so run them. */
+        nsh.run();
+      }
+    });
+  });
+})();
